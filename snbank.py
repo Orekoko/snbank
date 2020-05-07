720794f921e8e0882e2c6e613aaf8865e2d93b9e
Orekoko <adetimehinoreoluwa@gmail.com>
2020-05-07 18:45:19 +0100

    update changes

--- snbank.py
+++ snbank.py
@@ -1,11 +1,12 @@
 import json
+import random
 from datetime import datetime
 
 
 while True:
 	print('Welcome to Start.ng-Python-Task4 Bank')
 	print('Please enter 1 or 2 as shown below:')
-	login_choice = input("\t1 - Staff Login\n\t2 - Close App\n\t")
+	login_choice = input("\t1 - Staff Login\n\t2 - Close App\n\t\t")
 	while (login_choice != '1') and (login_choice != '2'):
 		login_choice = input('Enter the correct input (1 or 2): ')
 		
@@ -31,6 +32,42 @@ while True:
 	}
 	with open('user_session.txt', 'w') as file_object:
 		json.dump(session_data, file_object)
+	
+	print('Hello ' + username + '! what do you want to do? Enter 1, 2 or 3 to choose (see options below)')
+		
+	while True:
+		banking_choice = input("\t1 - Create new bank account\n\t2 - Check Account Details\n\t3 - Logout\n\t\t")
+		
+		while (banking_choice != '1') and (banking_choice != '2') and (banking_choice != '3'):
+			banking_choice = input('Wrong input enter 1, 2, or 3: ')
+		if banking_choice == '3':
+			break
+			
+		if banking_choice == '1':
+			print('I will be needing some data from you, so as to create an account for you')
+			numbers = '0123456789'
+			customer_banking_data = {}
+			customer_banking_data[username] = {}
+			customer_banking_data[username]['Account name'] = input("Enter an account name of your choice (Its best to use your firstname and lastname): ")
+			while True:
+				try:
+					customer_banking_data[username]['Opening Balance'] = float(input("\nEnter your opening amount. Just enter numbers only dont worry about putting the currency symbol, You'll get the type of account next: "))
+				except ValueError:
+					print('Wrong input!')
+					continue
+				else:
+					break
+			customer_banking_data[username]['Account Type'] = input("\nEnter the account type, enter like eg: 'Naira Savings Account', eg: 'Dollar Current Account': ")
+			customer_banking_data[username]['Account email'] = input("\nEnter the email address you want to associated with your account: ")
+			customer_banking_data[username]['Account Number'] = ''.join(random.choices(numbers, k=10))
+			with open ('customer.txt', 'a') as file_object:
+				json.dump(customer_banking_data, file_object)
+			print(customer_banking_data[username]['Account name'].title() + ', this is your account number: ' + customer_banking_data[username]['Account Number'])
+			print('So ' + username + ' what do you want to do next? enter 1, 2 or 3 to choose')
+			continue
+		if banking_choice == '2':
+			account_number = input('Enter your account number: ')
+			
 			
 		
 	
