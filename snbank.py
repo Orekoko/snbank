f7b9ef404ceb8bcfc202f72cf11203b18a9bc3cc
Orekoko <adetimehinoreoluwa@gmail.com>
2020-05-08 10:12:05 +0100

    update snbank

--- snbank.py
+++ snbank.py
@@ -1,4 +1,5 @@
 import json
+import os
 import random
 from datetime import datetime
 
@@ -41,33 +42,70 @@ while True:
 		while (banking_choice != '1') and (banking_choice != '2') and (banking_choice != '3'):
 			banking_choice = input('Wrong input enter 1, 2, or 3: ')
 		if banking_choice == '3':
+			os.remove('user_session.txt')
 			break
 			
 		if banking_choice == '1':
 			print('I will be needing some data from you, so as to create an account for you')
 			numbers = '0123456789'
-			customer_banking_data = {}
-			customer_banking_data[username] = {}
-			customer_banking_data[username]['Account name'] = input("Enter an account name of your choice (Its best to use your firstname and lastname): ")
+			account_name = input("Enter an account name of your choice (Its best to use your firstname and lastname): ")
 			while True:
 				try:
-					customer_banking_data[username]['Opening Balance'] = float(input("\nEnter your opening amount. Just enter numbers only dont worry about putting the currency symbol, You'll get the type of account next: "))
+					opening_balance = float(input("\nEnter your opening amount. Just enter numbers only dont worry about putting the currency symbol, You'll get the type of account next: "))
 				except ValueError:
-					print('Wrong input!')
+					print('Wrong Input!')
 					continue
 				else:
 					break
-			customer_banking_data[username]['Account Type'] = input("\nEnter the account type, enter like eg: 'Naira Savings Account', eg: 'Dollar Current Account': ")
-			customer_banking_data[username]['Account email'] = input("\nEnter the email address you want to associated with your account: ")
-			customer_banking_data[username]['Account Number'] = ''.join(random.choices(numbers, k=10))
-			with open ('customer.txt', 'a') as file_object:
-				json.dump(customer_banking_data, file_object)
-			print(customer_banking_data[username]['Account name'].title() + ', this is your account number: ' + customer_banking_data[username]['Account Number'])
-			print('So ' + username + ' what do you want to do next? enter 1, 2 or 3 to choose')
+			account_type = input("\nEnter the account type, enter like eg: 'Naira Savings Account', eg: 'Dollar Current Account': ")
+			account_email = input("\nEnter the email address you want to associated with your account: ")
+			account_number = ''.join(random.choices(numbers, k=10))
+			
+			customer_file_data = []
+			customer_banking_data = {
+			    username: [
+			        {
+			            'Account Name': account_name,
+			            'Opening Balance': opening_balance,
+			            'Account Type': account_type,
+			            'Account email': account_email,
+			            'Account Number': account_number,
+			        }
+			    ],
+			}
+			if os.stat('customer.txt').st_size == 0:
+				customer_file_data.append(customer_banking_data)
+				with open('customer.txt', 'w') as file_obj:
+					json.dump(customer_file_data, file_obj)
+				print(customer_banking_data[username][0]['Account Name'] + ' this is your account number: ' + customer_banking_data[username][0]['Account Number'])
+				print('So what do you want to do next? Enter 1, 2 or 3 to choose: ')
+			else:
+				with open('customer.txt') as file_obj:
+					data = json.load(file_obj)
+				data.append(customer_banking_data)
+				with open('customer.txt', 'w') as file_obj:
+					json.dump(data, file_obj)
+				print(customer_banking_data[username][0]['Account Name'] + ' this is your account number: ' + customer_banking_data[username][0]['Account Number'])
+				print('So what do you want to do next? Enter 1, 2 or 3 to choose: ')
 			continue
-		if banking_choice == '2':
-			account_number = input('Enter your account number: ')
-			
-			
 		
+		if banking_choice == '2':
+			collect_acct_no = input('Enter your account number so as to fetch your account details (please ensure you enter correctly: ')
+			check_account = {'Account Number': collect_acct_no,}
+			
+			with open('customer.txt') as file_obj:
+				data = json.load(file_obj)
+				found_flag = False
+			for user_data in data:
+				for user_data_key in user_data.keys():
+					for user_details in user_data[user_data_key]:
+						if check_account['Account Number'] in user_details.values():
+							found_flag = True
+							print('\nAccount Found ! See details below:')
+							for key, value in user_details.items():
+								print('\n' + key + ' : ' + str(value))
+								
+			if found_flag == False:
+				print('Account Not Found! You can register a new one if you wish')
+				continue
 	
